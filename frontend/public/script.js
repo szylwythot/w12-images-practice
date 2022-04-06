4b60d0feb43a430b346113593778c763a54531f5
toth_szilvia <szylwy@gmail.com>
2022-04-06 15:39:11 +0200

    change images

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,3 +1,27 @@
-window.addEventListener(`load`, () => {
+async function parseJSON(url){
+    const response = await fetch(url);
+    return response.json();
+}
+
+function swiperComponent(){
+    return `
+    <div class="swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">Slide 1</div>
+            <div class="swiper-slide">Slide 2</div>
+            <div class="swiper-slide">Slide 3</div>
+        </div>
+    </div>
+    `;
+}
+
+async function loadEventHandel() {
     console.log("Page is loaded.");
-});
+    const rootElement = document.getElementById("root");
+
+    rootElement.insertAdjacentHTML(`beforeend`, swiperComponent());
+
+    const result = await parseJSON("/image-list");
+}
+
+window.addEventListener(`load`, loadEventHandel);
