b9ebf103a98cda2e3d676339a8cfbd964bb4d48d
toth_szilvia <szylwy@gmail.com>
2022-04-06 15:24:40 +0200

    initial image list service endpoint

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+// const fileUpload = require('express-fileupload');
+
+const path = require('path');
+
+const fs = require('fs');
+const { request } = require('http');
+const app = express();
+
+const port = 9000;
+const ipAddress = `http://127.0.0.2:${port}`
+
+const frontendPath = path.join(`${__dirname}/../frontend`);
+
+app.use(express.json()); // opcionális
+app.use(`/pub`, express.static(`${frontendPath}/public`));
+
+app.get('/', (request, response, next) => { // ez a sima 
+    console.log();
+    response.sendFile(`${frontendPath}/index.html`);
+});
+
+app.get(`/image-list`, (request, response, next) => { 
+    response.sendFile(`${frontendPath}/data.json`);
+});
+
+app.listen(port, () => {
+    console.log(ipAddress);
+});
